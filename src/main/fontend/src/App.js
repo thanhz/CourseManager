24d924dd4d8e7c0a34db46dceccd6a089d8e12e6
Thanh <thanhz@hotmail.com>
2020-10-09 20:02:19 +0100

    Implemented usage of POST request

--- src/main/fontend/src/App.js
+++ src/main/fontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Users from "./Users"
 import Courses from './Courses'
--- src/main/fontend/src/Users.js
+++ src/main/fontend/src/Users.js
@@ -5,7 +5,9 @@ import axios from "axios";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
-  const [showResults, setShowResults] = useState(false);
+  const [showUsers, setUserResults] = useState(false);
+  const [showForms, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({name: ""});
 
   const fetchUsers = () => {
     axios.get("http://localhost:8080/users").then((response) => {
@@ -25,16 +27,47 @@ const Users = () => {
     });
   };
 
-  const onClick = () => {
-    setShowResults(!showResults);
-    fetchUsers();
+  const handleSubmit = () => {
+    axios.post("http://localhost:8080/users", formData).then(response =>
+      console.log(response)
+    ).catch(error =>
+      console.log(error)
+    )
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const onClick = (button) => {
+    if (button === "view") {
+      setUserResults(!showUsers);
+      if (showUsers === false) { //Dont fetch data again if hiding results
+        fetchUsers();
+      }
+    } else {
+      setShowForm(!showForms);
+    }
   };
 
   return (
     <div>
-      <input type='submit' value='View All Users' onClick={onClick} />
-      <input type='submit' value='Add a new user' onClick={onClick} />
-      {showResults ? <DisplayUsers /> : null}
+      <input type='submit' value='View All Users' onClick={() => onClick("view")} />
+      <input type='submit' value='Add a new user' onClick={() => onClick("add")} />
+      
+      {showForms ? (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      ) : null}
+      
+      {showUsers ? <DisplayUsers /> : null}
     </div>
   );
 };
